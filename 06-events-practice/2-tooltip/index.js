bb4972f959e189afcc16a8d748ca87066a6be01a
Boris Zhuravlev <boris.zhuravlev@ixcellerate.ru>
2023-10-15 01:51:29 +0300

    Task 6.2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,57 @@
 class Tooltip {
-  initialize () {
+	element;
+	constructor() {
+		if (!Tooltip.instance) {
+			Tooltip.instance = this;
+		}
+		return Tooltip.instance;
+	}
+	initialize() {
+		document.addEventListener('pointerover', this.onTooltipPointerOver);
+	}
+	onTooltipPointerOver = (event) => {
+		if (event.target.hasAttribute('data-tooltip')) {
+			const tooltip = document.querySelector('.tooltip');
+			if (!tooltip) {
+				this.render();
+			}
+			const tooltipText = event.target.dataset.tooltip;
+			this.element.textContent = tooltipText;
+			document.addEventListener('pointermove', this.onTooltipPointerMove);
+			event.target.addEventListener('pointerout', this.onTooltipPointerOut);
+		}
+	}
+	onTooltipPointerMove(event) {
+		const tooltip = document.querySelector('.tooltip');
+		tooltip.style.top = event.clientY + window.scrollY + 10 + 'px';
+		tooltip.style.left = event.clientX + 10 + 'px';
+	}
+	onTooltipPointerOut = (event) => {
+		if (event.target.hasAttribute('data-tooltip')) {
+			document.removeEventListener('pointermove', this.onTooltipPointerMove);
+			event.target.removeEventListener('pointerout', this.onTooltipPointerOut);
+			this.remove();
+		}
 
-  }
+	}
+	render() {
+		const wrapper = document.createElement('div');
+		wrapper.innerHTML = `<div class="tooltip"></div>`;
+		const element = wrapper.firstElementChild;
+		this.element = element;
+		document.body.append(this.element);
+	}
+	remove() {
+		if (this.element) {
+			this.element.remove();
+		}
+	}
+	destroy() {
+		document.removeEventListener('pointerover', this.onTooltipPointerOver);
+		document.addEventListener('pointerout', this.onTooltipPointerOut);
+		this.remove();
+		this.element = null;
+	}
 }
-
 export default Tooltip;
+
